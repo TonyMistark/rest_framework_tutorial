03829907703f9611a6d5ffd7b3c37107228db85a
ice <tony_mistark@163.com>
2017-02-19 17:43:01 +0800

    add bill

--- tutorial/quickstart/admin.py
+++ tutorial/quickstart/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from tutorial.quickstart.models import Bill
 
-# Register your models here.
+class BillAdmin(admin.ModelAdmin):
+    list_prr_page = 20
+    list_display = ("title", "ware", "cose")
+    
+admin.register(Bill, BillAdmin)
--- tutorial/quickstart/models.py
+++ tutorial/quickstart/models.py
@@ -1,3 +1,15 @@
+# coding=utf-8
+
 from django.db import models
 
-# Create your models here.
+class Bill(models.Model):
+    title = models.CharField(verbose_name="标题", max_length=500)
+    ware = models.CharField(verbose_name="商品", max_length=500)
+    cost = models.models.IntegerField(verbose_name="花费金额(单位分)")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "账单"
+        verbose_name_plural = "账单"
